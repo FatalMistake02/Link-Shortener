a2beaff37f742972f5552c05a0c1ef7fa3404a6f
Fatal <mistake02.fatal@gmail.com>
2025-06-17 19:39:03 -0400

    Moved firebase to .env

--- index.js
+++ index.js
@@ -5,8 +5,27 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-key.json');
 require('dotenv').config();
+//////////////////////////////////////////////
+
+// const serviceAccount = require('./firebase-key.json');
+
+const serviceAccount = {
+  type: process.env.firebase_type,
+  project_id: process.env.firebase_project_id,
+  private_key_id: process.env.firebase_private_key_id,
+  private_key: process.env.firebase_private_key.replace(/\\n/g, '\n'),
+  client_email: process.env.firebase_client_email,
+  client_id: process.env.firebase_client_id,
+  auth_uri: process.env.firebase_auth_uri,
+  token_uri: process.env.firebase_token_uri,
+  auth_provider_x509_cert_url: process.env.firebase_auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.firebase_client_x509_cert_url,
+  universe_domain: process.env.firebase_universe_domain
+};
+
+/////////////////////////////////////////////
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -39,7 +58,7 @@ function isValidUrl(url) {
   }
 }
 function isBlocked(url) {
-  const blockedKeywords = [''];
+  const blockedKeywords = [];
   return blockedKeywords.some(keyword => url.toLowerCase().includes(keyword));
 }
 
